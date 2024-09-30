b35520628ae43909068e7b1b5c0cbb54f20e64dd
Tao <ginadrcoder@gmail.com>
2024-09-30 20:46:01 +1000

    feat(css):add tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: [
+    './public/**/*.{html,js}',
+    './components/**/*.{html,js}',
+    './public/index.html',
+    './src/**/*.{html,js}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/public/assets/carrots.jpg')",
+      }
+    },
+    fontFamily: {
+      'sans': ['Cupidus', 'Helvetica', 'sans-serif'],
+      'serif': ['Merriweather', 'serif'],
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ], 
 }
-
